eb1f3de81ca49fc78021d46ed7584351b7d159ae
NabidAlam <msa_nabid007@hotmail.com>
2019-01-22 17:23:56 +0600

    added input form

--- Angular7/src/app/app.module.ts
+++ Angular7/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PaymentDetailsComponent } from './payment-details/payment-details.component';
@@ -17,7 +17,8 @@ import { PaymentDetailService } from './shared/payment-detail.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [PaymentDetailService],
   bootstrap: [AppComponent]
