32f2f6bebcd0092cd76f33e57353d4cb888e8be7
NabidAlam <msa_nabid007@hotmail.com>
2019-01-22 15:18:20 +0600

    Controller changes

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -28,7 +28,7 @@ namespace WebAPI
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.AddDbContext<PaymentDetailContext>(options=>options.UseSqlServer(Configuration.GetConnectionString("DevConnection"))); 
-
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
