3cfe1d7d0a8c525bab44c2d5fb41178d356e89bc
NabidAlam <msa_nabid007@hotmail.com>
2019-01-23 10:08:27 +0600

    textbox input form angular js

--- Angular7/src/app/app.module.ts
+++ Angular7/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
 import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
+// import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PaymentDetailsComponent } from './payment-details/payment-details.component';
 import { PaymentDetailComponent } from './payment-details/payment-detail/payment-detail.component';
@@ -17,7 +18,7 @@ import { PaymentDetailService } from './shared/payment-detail.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    // AppRoutingModule,
     FormsModule
   ],
   providers: [PaymentDetailService],
